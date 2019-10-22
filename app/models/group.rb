e4e74ac28d5e93d48dd292c0d046607749cf4f57
fujicho <nnnn.sohei.fujita@gmail.com>
2019-10-22 16:47:49 +0900

    サイドバーに最新メッセージを表示

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,14 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :messages
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      # 変数定義をしつつ、条件式を記述している
+      last_message.content? ? last_message.content  : '画像が投稿されています'
+      # 三項演算子　条件式 ? trueの時の値 : falseの時の値
+    else
+      'まだメッセージはありません'
+    end
+  end
 end
