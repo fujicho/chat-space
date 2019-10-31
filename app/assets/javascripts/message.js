42c11bdeb2141b6e7ec31caf7af7e2dd2646b522
fujicho <nnnn.sohei.fujita@gmail.com>
2019-10-31 21:07:31 +0900

    Ajaxの実装

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,50 @@
+$(document).on('turbolinks:load',function(){
+  function buildHTML(message){
+    var content = message.content ? `${message.content}` : "";
+    var img = message.image ? `<img src= ${message.image }>` : "";
+    var html = `<div class="message">
+    <div class="top-message">
+      <div class="top-message__user-name">
+        ${message.user_name}
+      </div>
+      <div class="top-message__date">
+        ${message.date}
+      </div>
+    </div>
+    <div class="bottom-message">
+      <div class="bottom-message__content">
+        ${content}
+      </div>
+      <div class='lower-message__image'>
+        ${img}
+      </div>
+    </div>`
+    return html;
+  }
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    var message = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: message,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('#message_content').val('')
+      $('.messages').animate({ scrollTop: $(".messages")[0].scrollHeight }, 1200);
+      $('#message_image').val('')
+    })
+    .fail(function(data){
+      alert('エラー');
+    })
+    .always(function(data){
+      $('.form__submit').prop('disabled', false);
+    })
+  });
+});
